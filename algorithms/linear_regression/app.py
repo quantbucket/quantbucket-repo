9f7f371dae5c16f70f0e2e842c8b47f822833d7e
Leonardo Aranda <leonardoaranda@gmail.com>
2015-05-07 20:13:02 -0300

    Fix linear_regression

--- algorithms/linear_regression/app.py
+++ algorithms/linear_regression/app.py
@@ -5,8 +5,8 @@ import numpy as np
 class Application(Algorithm):
 
 	def main(self):
-		x = self.data(dataset_fields('x_axis')[0])
-		y = self.data(dataset_fields('y_axis')[0])
+		x = self.data(self.dataset_fields('x_axis')[0])
+		y = self.data(self.dataset_fields('y_axis')[0])
 		slope, intercept, r_value, p_value, std_err = stats.linregress(x,y)
 		doc = {
 			'slope' : slope,
