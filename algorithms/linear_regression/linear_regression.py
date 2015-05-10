69705a7a5a834045f6ef6e6eac76fb5403a192d3
Leonardo Aranda <leonardoaranda@gmail.com>
2015-05-10 16:46:10 -0300

    Fixes to isotonic regression

---
+++ algorithms/linear_regression/linear_regression.py
@@ -0,0 +1,20 @@
+from quantbucket import Base
+from scipy import stats
+import numpy as np
+
+class Application(Base):
+
+	def main(self):
+		x_axis_field = self.fields_by_key('x_axis')[0]
+		y_axis_field = self.fields_by_key('y_axis')[0]
+		x = self.slice_data(x_axis_field,int)
+		y = self.slice_data(y_axis_field,int)
+		slope, intercept, r_value, p_value, std_err = stats.linregress(x,y)
+		doc = {
+			'slope' : slope,
+			'intercept'  : intercept,
+			'r_value' : r_value,
+			'p_value' : p_value,
+			'std_err' : std_err
+		}
+		return doc
--- visualizations/isotonic_regression/isotonic_regression.py
+++ visualizations/isotonic_regression/isotonic_regression.py
@@ -13,8 +13,9 @@ class Chart(Base):
 	def main(self):
 		x_field = self.fields_by_key('x')[0]
 		y_field = self.fields_by_key('y')[0]	
-		x = self.slice_data(x_field,int)
-		y = self.slice_data(y_field,int)
+		x = np.array(self.slice_data(x_field,int))
+		y = np.array(self.slice_data(y_field,int))
+		n = len(x)
 		render = StringIO.StringIO()
 		
 		###############################################################################
